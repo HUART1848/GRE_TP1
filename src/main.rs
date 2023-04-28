a108b43065d43a475cd3b42f9a77e86fa9c180c1
Hugo Huart <hugo.huart@heig-vd.ch>
2023-04-28 10:35:47 +0200

    matrix: initial struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+#[derive(Debug)]
+struct SquareMatrix {
+    width: usize,
+    data: Vec<u32>
+}
+
+impl SquareMatrix {
+    fn new(width: usize) -> Self {
+        let data:Vec<u32> = vec![0; width * width];
+        return SquareMatrix { width, data };
+    }
+
+    fn size(&self) -> usize {
+        return self.width * self.width;
+    }
+
+    fn get(&self, i: usize, j: usize) -> u32 {
+        return self.data[i * self.width + j];
+    }
+
+    fn set(&mut self, i: usize, j: usize, value: u32) {
+        self.data[i * self.width + j] = value;
+    }
+
+    fn show(&self) {
+        for i in 0..self.width {
+            for j in 0..self.width {
+                print!("{} ", self.get(i, j));
+            }
+            println!();
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut m = SquareMatrix::new(4);
+    m.set(0, 3, 12);
+    m.set(2, 1, 440);
+    m.show();
 }
